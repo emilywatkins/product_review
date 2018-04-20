74c467e574388afec269fc2f154a02e0f2165326
Emily Watkins <emily.e.dolliver@gmail.com>
2018-04-20 14:45:47 -0700

    add add review functionality. issues with test identifying fields, will come back to testing in order to prioritize functionality.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,6 @@
-class ProductsController < ApplicationController
-
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
 end
