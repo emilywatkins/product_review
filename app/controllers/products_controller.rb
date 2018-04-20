1aa5096890050485bbc45a854491bc94078b13a2
Emily Watkins <emily.e.dolliver@gmail.com>
2018-04-20 16:45:26 -0700

    fix flash messages.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to product_path(@product)
+      redirect_to products_path
       flash[:notice] = 'updated product'
     else
       render :edit
