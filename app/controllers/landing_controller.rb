3e23bbcd1854fc8c417cc41aeaaf8d1697a05828
Emily Watkins <emily.e.dolliver@gmail.com>
2018-04-20 16:38:01 -0700

    add scopes for product with most reviews and most recently added products to show on landing.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,7 @@
 class LandingController < ApplicationController
   def index
     @domestic = Product.domestic
+    @newest = Product.newest
+    @most_reviewed = Product.most_reviewed
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,12 @@ class Product < ActiveRecord::Base
   validates :country_of_origin, :presence => true
 
   scope :domestic, -> { where("country_of_origin =?", 'USA') }
+  scope :newest, -> { order(created_at: :desc).limit(3) }
+  scope :most_reviewed, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
 end
