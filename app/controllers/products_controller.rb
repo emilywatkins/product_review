63630b65aaa303156ed1286d692e093f860f04aa
Emily Watkins <emily.e.dolliver@gmail.com>
2018-04-20 12:51:26 -0700

    add capybara gem, add integration spec for add products path, fails.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,22 @@
 class ProductsController < ApplicationController
-  def index
+  def index # /products
     @products = Product.all
   end
 
-  def show # products/:id
+  def show # /products/:id
     @product = Product.find(params[:id])
+  end
+
+  def new # /products/new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 end
---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'add a new product'
+    fill_in 'Name', :with => 'Eggs'
+    fill_in 'Cost', :with => 2
+    fill_in 'Country of origin', :with => 'USA'
+    click_on 'Create Product'
+    expect(page).to have_content 'Eggs'
+  end
+
+end
